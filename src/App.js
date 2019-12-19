b6a37dd8df0dae2b01ba32a5b893c9010b35690d
root <1046166@qq.com>
2019-12-19 09:33:14 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -9,19 +9,15 @@ import ZombieAttack from "./ZombieAttack";
 import ContractAdmin from "./ContractAdmin";
 import MyWeb3 from './MyWeb3'
 import {
-    BrowserRouter as 
-    Router,
+    BrowserRouter as Router,
     Route,
-    Link,
-    BrowserRouter
+    Link
   } from "react-router-dom"
 
 class App extends Component {
     constructor(props) {
         super(props);
-        const _pathname = window.location.pathname.split('/')
-        const pathname = '/'+_pathname[_pathname.length-2]
-        this.state = { AdminArea:()=>{return(<Fragment></Fragment>)},pathname:pathname }
+        this.state = { AdminArea:()=>{return(<Fragment></Fragment>)} }
     }
     
     componentDidMount(){
@@ -52,55 +48,52 @@ class App extends Component {
     }
     
     render() { 
-        console.log(this.state)
         let AdminArea = this.state.AdminArea
         return (
             <Fragment>
                 <Router>
-                    <BrowserRouter basename={this.state.pathname}>
-                        <section className="zombies-hero no-webp block app-block-intro pt-5 pb-0">
-                            <div className="container">
-                                <div className="menu">
-                                    <ul>
-                                        <li>
-                                            <button className="start-course-btn">
-                                                <span><Link to="/">僵尸军团</Link></span>
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="start-course-btn">
-                                                <span><Link to="/MyZombie">我的僵尸</Link></span>
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="start-course-btn">
-                                                <span><Link to="/ZombieMarket">僵尸市场</Link></span>
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button className="start-course-btn">
-                                                <span><Link to="/ZombieSimulator">基因模拟</Link></span>
-                                            </button>
-                                        </li>
-                                        <AdminArea></AdminArea>
-                                    </ul>
-                                </div>
+                    <section className="zombies-hero no-webp block app-block-intro pt-5 pb-0">
+                        <div className="container">
+                            <div className="menu">
+                                <ul>
+                                    <li>
+                                        <button className="start-course-btn">
+                                            <span><Link to="/">僵尸军团</Link></span>
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button className="start-course-btn">
+                                            <span><Link to="/MyZombie">我的僵尸</Link></span>
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button className="start-course-btn">
+                                            <span><Link to="/ZombieMarket">僵尸市场</Link></span>
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button className="start-course-btn">
+                                            <span><Link to="/ZombieSimulator">基因模拟</Link></span>
+                                        </button>
+                                    </li>
+                                    <AdminArea></AdminArea>
+                                </ul>
                             </div>
-                        </section>
-                        <section className="zombie-container block bg-walls no-webp">
-                            <div className="container">
-                                <div className="area">
-                                    <Route exact path="/" component={ZombieArmy}></Route>
-                                    <Route path="/MyZombie" component={MyZombie}></Route>
-                                    <Route path="/ZombieMarket" component={ZombieMarket}></Route>
-                                    <Route path="/ZombieSimulator" component={ZombieSimulator}></Route>
-                                    <Route path="/ZombieDetail" component={ZombieDetail}></Route>
-                                    <Route path="/ZombieAttack" component={ZombieAttack}></Route>
-                                    <Route path="/ContractAdmin" component={ContractAdmin}></Route>
-                                </div>
+                        </div>
+                    </section>
+                    <section className="zombie-container block bg-walls no-webp">
+                        <div className="container">
+                            <div className="area">
+                                <Route exact path="/" component={ZombieArmy}></Route>
+                                <Route path="/MyZombie" component={MyZombie}></Route>
+                                <Route path="/ZombieMarket" component={ZombieMarket}></Route>
+                                <Route path="/ZombieSimulator" component={ZombieSimulator}></Route>
+                                <Route path="/ZombieDetail" component={ZombieDetail}></Route>
+                                <Route path="/ZombieAttack" component={ZombieAttack}></Route>
+                                <Route path="/ContractAdmin" component={ContractAdmin}></Route>
                             </div>
-                        </section>
-                    </BrowserRouter>
+                        </div>
+                    </section>
                 </Router>
             </Fragment>
             );
