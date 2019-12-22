7c22b4278f281ef88e7657cadf0e5c1d4ebc8ea0
root <1046166@qq.com>
2019-12-23 00:13:49 +0800

    update

--- src/ZombieArmy.js
+++ src/ZombieArmy.js
@@ -19,6 +19,7 @@ class ZombieArmy extends Component {
         //console.log(window.web3._extend.utils)
         let that = this
         let ethereum = window.ethereum
+        console.log(ethereum)
         if (typeof ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
             MyWeb3.init().then(function(res){
                 that.zombieArmy()
